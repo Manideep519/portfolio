b2602939ed720202d893cb45e1ac6006c5c51e90
manideep519 <manideepyadav359@gmail.com>
2020-12-12 16:56:12 +0530

    mobile styles fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
     // Add smooth scrolling to all links
-    $(".nav side-menu__nav ul li").on('click', function(event) {
+    $("a").on('click', function(event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+        if ($(window).width() > 768) {
 
-            // Store hash
-            var hash = this.hash;
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
+                // Store hash
+                var hash = this.hash;
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body, section').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function() {
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        }
+
     });
 });
 
